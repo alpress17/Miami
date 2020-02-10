8896c42b2226c5f88a38912f257099d329edf4ed
Alex <al.press17@gmail.com>
2020-02-10 14:30:11 +0200

    add gamburger; finished adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+	/*  ================================= HAMBURGER ================================= */
+	window.addEventListener("DOMContentLoaded", () => {
+		const menu = document.querySelector(".menu"),
+			menuItem = document.querySelectorAll(".menu__item"),
+			hamburger = document.querySelector(".hamburger");
+
+		hamburger.addEventListener("click", () => {
+			hamburger.classList.toggle("hamburger_active");
+			menu.classList.toggle("menu_active");
+		});
+
+		menuItem.forEach(item => {
+			item.addEventListener("click", () => {
+				hamburger.classList.toggle("hamburger_active");
+				menu.classList.toggle("menu_active");
+			});
+		});
+	});
 	/*  ====================== Smooth scroll for navigate menu ====================== */
-	$("#menu").on("click", "a", function(event) {
+	$("#navigate").on("click", "a", function(event) {
 		event.preventDefault();
 		const id = $(this).attr("href"),
 			//узнаем высоту от начала страницы до блока на который ссылается якорь
